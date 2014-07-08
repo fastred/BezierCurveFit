dc3cf11bf28a44fea901750940545f7653f70d6b
Arkadiusz Holko <fastred@fastred.org>
2014-07-08 13:53:21 +0200

    Initial Commit

---
+++ BezierCurveFit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BezierCurveFit
+//
+//  Created by Arkadiusz on 08-07-14.
+//  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
