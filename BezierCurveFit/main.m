d5bff64b89aa18f8c6390eb9490076a0880f25d3
Arkadiusz Holko <fastred@fastred.org>
2014-07-09 18:32:08 +0200

    Improve memory usage by adding autoreleasepool

--- BezierCurveFit/main.m
+++ BezierCurveFit/main.m
@@ -74,20 +74,23 @@ int main(int argc, const char * argv[])
         // Finds a cubic Bezier curve with the smallest error.
         // You may need to adjust the errorTreshold starting point and step if you're trying to fit a curve with a bounding box different than [(0,0) (100, 100)].
         for (float errorTreshold = 0; true; errorTreshold += 0.1) {
-            NSBezierPath *newPath = [path fb_fitCurve:errorTreshold];
-            if ([newPath elementCount] == 2) {
-                for (int i = 0; i < [newPath elementCount]; i++) {
 
-                    NSPoint pointArray[3];
-                    NSBezierPathElement element = [newPath elementAtIndex:i associatedPoints:pointArray];
+            @autoreleasepool {
+                NSBezierPath *newPath = [path fb_fitCurve:errorTreshold];
+                if ([newPath elementCount] == 2) {
+                    for (int i = 0; i < [newPath elementCount]; i++) {
 
-                    if (element == NSMoveToBezierPathElement) {
-                        NSLog(@"Move to Point: %@", NSStringFromPoint(pointArray[0]));
-                    } else if (element == NSCurveToBezierPathElement) {
-                        NSLog(@"Curve;\nControl Point 1: %@,\nControl Point 2: %@,\nEnd Point: %@", NSStringFromPoint(pointArray[0]), NSStringFromPoint(pointArray[1]), NSStringFromPoint(pointArray[2]));
+                        NSPoint pointArray[3];
+                        NSBezierPathElement element = [newPath elementAtIndex:i associatedPoints:pointArray];
+
+                        if (element == NSMoveToBezierPathElement) {
+                            NSLog(@"Move to Point: %@", NSStringFromPoint(pointArray[0]));
+                        } else if (element == NSCurveToBezierPathElement) {
+                            NSLog(@"Curve;\nControl Point 1: %@,\nControl Point 2: %@,\nEnd Point: %@", NSStringFromPoint(pointArray[0]), NSStringFromPoint(pointArray[1]), NSStringFromPoint(pointArray[2]));
+                        }
                     }
+                    break;
                 }
-                break;
             }
         }
     }
